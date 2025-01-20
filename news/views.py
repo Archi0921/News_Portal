9e1ff97c8e38ec35e78d25d73b7835276e69f894
Archi0921 <Art2006101@ya.ru>
2025-01-20 10:29:24 +0500

    Add view detail

--- news/views.py
+++ news/views.py
@@ -1,9 +1,14 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import *
 
 class PostList(ListView):
     model = Post
     template_name = 'posts.html'
     context_object_name = 'posts'
+
+class PostDetail(DetailView):
+    model =Post
+    template_name = 'post.html'
+    context_object_name = 'post'
 
 
