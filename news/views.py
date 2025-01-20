7e5ed36972f52a00206e06aa417424921194633e
Archi0921 <Art2006101@ya.ru>
2025-01-20 20:19:27 +0500

    new view

--- news/views.py
+++ news/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.views.generic import ListView, DetailView
 from .models import *
 
@@ -5,6 +7,12 @@ class PostList(ListView):
     model = Post
     template_name = 'posts.html'
     context_object_name = 'posts'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['next_sale'] = None
+        return context
 
 class PostDetail(DetailView):
     model =Post
